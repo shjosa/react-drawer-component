91bf4372d656e26bbb343b24a6aff2c06ccb14cd
shjosa <shjosapak@gmail.com>
2022-07-13 18:33:37 -0400

    hovering highlighting with interesting functionality

--- src/pages/Main.styles.tsx
+++ src/pages/Main.styles.tsx
@@ -4,10 +4,17 @@ export const Body = styled.div`
 width: 20%;
 /*border: 1px solid #444;*/
 min-height: 100vh;
-background-color: grey;
+background-color: #f5f5f5;
 padding: auto;
 `;
 
-export const Item = styled.div`
-    padding-left: 1rem;
+export const Item = styled.div<{ isChild: number | undefined }>`
+    padding-left: ${props => (props.isChild === undefined ? 1 : props.isChild + 1)}rem;
+    padding-top: 0.5rem;
+    padding-bottom: 0.5rem;
+    font-family: Arial, sans-serif;
+    font-size: 1.2rem;
+    &:hover {
+        background-color: lightgrey;
+    };
 `;
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,28 +1,36 @@
-import React, { PropsWithChildren } from 'react';
+import React, { useState } from 'react';
 import { JSXElementConstructor } from 'react';
+import { setConstantValue } from 'typescript';
 import { Header } from '../components/Header';
 import { Body, Item } from './Main.styles';
 
 interface SidebarItemProps {
     children?: JSX.Element;
     text: string;
-    isChild?: boolean;
+    isChild?: number;
 }
 
 const SidebarItem = ({ children, text, isChild }: SidebarItemProps) => {
 
+    const [state, setState] = useState("enabled");
+
     const childrenArr = React.Children.toArray(children).map((child) =>
         React.cloneElement(child as React.ReactElement<SidebarItemProps, string | JSXElementConstructor<any>>, {
-          isChild: true,
+          isChild: isChild === undefined ? 1 : isChild + 1,
         })
     );
 
+    function editState(status: string, text: string) {
+        setState(status);
+        console.log(text + ": " + status);
+    };
+
     return (
         <>
-            <Item>
+            <Item isChild={isChild} onMouseEnter={() => console.log(text + " " + isChild)}>
                 <div>{text}</div>
-                {children}
             </Item>
+            {childrenArr}
         </>
         
     );
