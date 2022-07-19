d85a23bc0e3d5f917d3d73a9da1042eda7a01c80
shjosa <shjosapak@gmail.com>
2022-07-19 15:45:14 -0400

    added icons, CollapsableSidebarItem, fixed spacing

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -2,16 +2,18 @@ import React, { useState } from 'react';
 import { JSXElementConstructor } from 'react';
 import { Header } from '../components/Header';
 import { Body, Item } from './Main.styles';
+import { MdKeyboardArrowDown, MdKeyboardArrowUp } from "react-icons/md";
+import { IconType } from 'react-icons';
 
 interface SidebarItemProps {
     text: string;
     paddingSize?: number;
-    icon?: string;
+    icon?: JSX.Element;
     onClick?: () => void;
 }
 
 const SidebarItem = ({ children, text, paddingSize, icon, onClick }: React.PropsWithChildren<SidebarItemProps>) => {
-
+    
     const childrenArr = React.Children.toArray(children).map((child) =>
         React.cloneElement(child as React.ReactElement<SidebarItemProps, string | JSXElementConstructor<any>>, {
           paddingSize: paddingSize === undefined ? 1 : paddingSize + 1,
@@ -22,7 +24,7 @@ const SidebarItem = ({ children, text, paddingSize, icon, onClick }: React.Props
         <>
             <Item paddingSize={paddingSize} onClick={onClick}>
                 <span>{text}</span>
-                { icon && <span>{icon}</span> }
+                { icon }
             </Item>
             {childrenArr}
         </>
@@ -33,25 +35,31 @@ const SidebarItem = ({ children, text, paddingSize, icon, onClick }: React.Props
 const CollapsableSidebarItem = ({ children, text, paddingSize }: Omit<React.PropsWithChildren<SidebarItemProps>, 'icon' | 'onClick'>) => {
     const [expanded, setExpanded] = useState(false);
 
-    return <SidebarItem icon={expanded ? 'e' : 'c'} text={text} onClick={() => setExpanded(v => !v)}>
-        {expanded && <>
+    return <SidebarItem icon={expanded ? <MdKeyboardArrowDown /> : <MdKeyboardArrowUp />} text={text} onClick={() => setExpanded(v => !v)} paddingSize={paddingSize} children={expanded && children} />
+}
+
+const Sidebar = ({ children }: React.PropsWithChildren) => {
+    return (
+        <>
             {children}
-        </>}
-    </SidebarItem>
+        </>
+    )
 }
 
 export const Main = () => {
     return (
         <Body>
-            <Header />
-            <CollapsableSidebarItem text="Primary">
-                <SidebarItem text="Secondary">
-                    <SidebarItem text="Tertiary"></SidebarItem>
-                </SidebarItem>
-            </CollapsableSidebarItem>
-            <SidebarItem text="Primary">
-                <SidebarItem text="Secondary"></SidebarItem>
-            </SidebarItem>
+            <Sidebar>
+                <Header />
+                <CollapsableSidebarItem text="Primary">
+                    <CollapsableSidebarItem text="Secondary">
+                        <SidebarItem text="Tertiary"></SidebarItem>
+                    </CollapsableSidebarItem>
+                </CollapsableSidebarItem>
+                <CollapsableSidebarItem text="Primary">
+                    <SidebarItem text="Secondary"></SidebarItem>
+                </CollapsableSidebarItem>
+            </Sidebar>
         </Body>
     )
 }
